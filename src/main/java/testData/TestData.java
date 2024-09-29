00f408683914107b549370c0fd2e589dbe2a4da0
Denys Leonov <skydark912@gmail.com>
2024-09-29 20:32:11 +0300

    test cases created

---
+++ src/main/java/testData/TestData.java
@@ -0,0 +1,8 @@
+package testData;
+
+public class TestData {
+    public static final String FIRST_NAME = "Denys";
+    public static final String LAST_NAME = "Leonov";
+    public static final String USER_NAME = "test_user";
+    public static final String PASSWORD = "123456Qwerty!";
+}
--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -3,8 +3,11 @@ package baseTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import testData.TestData;
 import utils.Utils;
 
 import java.time.Duration;
@@ -22,11 +25,16 @@ public class BaseTest {
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         logger.info("Browser opened");
 
-        webDriver.get("https://www.youtube.com/");
+        webDriver.get("https://demoqa.com/login");
 
-        Utils.waitABit(5);
-        logger.info("Open youtube.com");
+        WebElement userNameField =  webDriver.findElement(By.xpath("//input[@placeholder='UserName']"));
+        WebElement passwordField =  webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
+        WebElement loginButton = webDriver.findElement(By.id("login"));
+        userNameField.sendKeys(TestData.USER_NAME);
+        passwordField.sendKeys(TestData.PASSWORD);
         logger.info("check title: " + webDriver.getTitle());
+        loginButton.click();
+        Utils.waitABit(2);
         webDriver.quit();
     }
 }
